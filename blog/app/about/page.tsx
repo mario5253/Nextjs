d3ac276af40620b846d822d02006fb5d3325bacc
mario5253 <c0073641@gmail.com>
2026-06-01 22:25:02 +0900

    今日の積み上げ

--- blog/app/about/page.tsx
+++ blog/app/about/page.tsx
@@ -1,31 +1,52 @@
 import Hero from "@/components/Hero"
 import Container from "@/components/Container"
 import PostBody from "@/components/PostBody"
-
+import Contact from "@/components/Contact"
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from "@/components/two-column"
+import Image from 'next/image'
+import eyecatch from '../../images/Copilot_20260601_221714.png'
 
 export default function About() {
   return (
     <Container>
-      <PostBody>
-        <Hero
+      <Hero
           title = "About"
           subtitle="About development activities"
         />
-        <h2>モノづくりで目指している事</h2>
-        <p>
-          ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト
-          ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト
-        </p>
-        <h3>モノづくりで目指している事</h3>
-        <p>
-          ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト
-          ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト
-        </p>
-        <p>
-          ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト
-          ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト
-        </p>
-      </PostBody>
+
+        <figure>
+          <Image
+          src={eyecatch}
+          alt=""
+          layout="responsive"
+          sizes="(min-width: 1152px) 1152px, 100vw"
+          priority
+          placeholder="blur"
+          />
+        </figure>
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <h2>モノづくりで目指している事</h2>
+              <p>
+                ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト
+                ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト
+              </p>
+              <h3>モノづくりで目指している事</h3>
+              <p>
+                ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト
+                ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト
+              </p>
+              <p>
+                ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト
+                ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト
+              </p>
+            </PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar>
+            <Contact />
+          </TwoColumnSidebar>
+        </TwoColumn>
     </Container>
   ) 
 }
